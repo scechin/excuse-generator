937a7a9e2f05cfbfff82152c49793af2993948ab
scechin <sircechin@yahoo.com>
2020-05-15 19:54:18 +0000

    completing excuse generator

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import random
 import math
 
 # your code here
+
+excuse = 'The dog eat my homework when I finished'
+who = ['the dog','my granma','his turtle','my bird']
+what = ['eat','pissed','crushed','broked']
+when = ['before the class','right in time','when I finished','during my lunch','while I was praying']
+
+print(who[getrandomnumber()] + " " + what[getrandomnumber()] + " " + when[getrandomnumber()])
+def getrandomnumber():
+    return random.randint()
